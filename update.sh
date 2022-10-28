464a42876da5ba6894a3dac0074a5c42e1919bef
PaintOnBrush <ptrakk@gmail.com>
2022-10-27 17:25:55 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in $(cat <( curl https://dumps.wikimedia.org/enwiki/latest/ | grep 'href="enwiki' | cut -d '"' -f 2 ) ) ; do echo $i; [[ -f $i ]] && curl -z $i https://dumps.wikimedia.org/enwiki/latest/$i -o $i || curl https://dumps.wikimedia.org/enwiki/latest/$i -o $i  ;done
+for i in $(cat <( curl https://dumps.wikimedia.org/enwiki/latest/ | grep 'href="enwiki' | cut -d '"' -f 2 ) ) ; do echo -e \\n'-------------------------------------------------------------------------------'\\n\\n$i; [[ -f $i ]] && curl -z $i https://dumps.wikimedia.org/enwiki/latest/$i -o $i || curl https://dumps.wikimedia.org/enwiki/latest/$i -o $i  ;done
