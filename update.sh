d4ee211af8e469cc24db109658bdba1a6df1bf37
PaintOnBrush <ptrakk@gmail.com>
2022-10-27 17:17:09 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -N -i <( for i in $(cat <( wget -qO- https://dumps.wikimedia.org/enwiki/latest/ ) | grep a\ href=\"enwiki   | cut -d '"' -f 2); do echo 'https://dumps.wikimedia.org/enwiki/latest/'$i; done )
+for i in $(cat <( curl https://dumps.wikimedia.org/enwiki/latest/ | grep 'href="enwiki' | cut -d '"' -f 2 ) ) ; do echo $i; [[ -f $i ]] && curl -z $i https://dumps.wikimedia.org/enwiki/latest/$i -o $i || curl https://dumps.wikimedia.org/enwiki/latest/$i -o $i  ;done
